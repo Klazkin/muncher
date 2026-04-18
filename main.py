bac25357190aadc1666a4037228542588ad44e63
klazkin <matvei.matpac@gmail.com>
2026-04-18 20:23:59 +0300

    better organization

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-def compile_blueprint():
-    import os
-
-    print("starting compile")
-
-    os.system("rm main.ui")
-    os.system("blueprint-compiler compile main.blp >> main.ui")
-
-    print("finished compilation")
-
-
-compile_blueprint()
-
-import re
+import asyncio
+import os
 import subprocess
 import sys
-from operator import sub
-from os import environ
-from syslog import LOG_DAEMON
 
 import gi
+import minecraft_launcher_lib as mc
 from dotenv import load_dotenv
+from gi.repository import Adw, Gio, GLib, Gtk
+
+assert load_dotenv()
+client_id = os.environ["CLIENT_ID"]
+redirect_uri = os.environ["REDIRECT_URI"]
+
+print("Starting compilation")
+
+os.system("rm main.ui")
+os.system("blueprint-compiler compile main.blp >> main.ui")
+
+print("Finished compilation")
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
-
-from gi.repository import Adw, Gio, GLib, Gtk
 
 
 class Application(Adw.Application):
@@ -36,28 +32,41 @@ class Application(Adw.Application):
         )
 
     def do_activate(self):
-        self.window: Adw.ApplicationWindow = SampleWindow(application=self)
+        self.window: Adw.ApplicationWindow = MuncherWindow(application=self)
         self.window.present()
 
 
 @Gtk.Template(filename="main.ui")
-class SampleWindow(Adw.ApplicationWindow):
-    __gtype_name__ = "SampleWindow"
+class MuncherWindow(Adw.ApplicationWindow):
+    __gtype_name__ = "MuncherWindow"
 
-    label = Gtk.Template.Child()
+    button_play = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.button_play.connect("clicked", on_play_pressed)
+
+
+def on_play_pressed(_):
+    minecraft_directory = mc.utils.get_minecraft_directory()
+    latest_version = mc.utils.get_latest_version()["release"]
+
+    mc.install.install_minecraft_version(latest_version, minecraft_directory)
+
+    options = mc.utils.generate_test_options()
+
+    minecraft_command = mc.command.get_minecraft_command(
+        latest_version, minecraft_directory, options
+    )
+
+    completed = subprocess.run(minecraft_command, start_new_session=True)
+    print("process finished with", completed)
+
 
 def login():
-    client_id = environ["CLIENT_ID"]
-    redirect_uri = environ["REDIRECT_URI"]
-
     login_url = mc.microsoft_account.get_login_url(client_id, redirect_uri)
-
-    print(login_url)
-    #
+    print("go to:", login_url)
     redirect_url = input("redirect_url:")
 
     if not mc.microsoft_account.url_contains_auth_code(redirect_url):
@@ -72,34 +81,9 @@ def login():
         client_id, None, redirect_uri, str(auth_code), None
     )
 
+    return login_result
+
 
 if __name__ == "__main__":
     app = Application()
     app.run(sys.argv)
-
-    exit(0)
-
-    assert load_dotenv()
-
-    import minecraft_launcher_lib as mc
-
-    # Get latest version
-    #
-    minecraft_directory = mc.utils.get_minecraft_directory()
-
-    latest_version = mc.utils.get_latest_version()["release"]
-
-    print(minecraft_directory, latest_version)
-
-    mc.install.install_minecraft_version(latest_version, minecraft_directory)
-
-    options = mc.utils.generate_test_options()
-
-    minecraft_command = mc.command.get_minecraft_command(
-        latest_version, minecraft_directory, options
-    )
-
-    completed = subprocess.run(minecraft_command)
-
-    print("process finished with", completed)
-    # print(minecraft_command)
